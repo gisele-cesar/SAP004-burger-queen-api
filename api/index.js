b13b078219c8c5edd83fa30db2cf33e8384168b0
Gisele Cesar <gisele-cesar@hotmail.com>
2020-08-26 23:03:54 -0300

    inclusão de rotas para products e orders

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import productRoutes from '../api/server/src/routes/ProductRoutes'
+import orderRoutes from '../api/server/src/routes/OrderRoutes'
 
 const app = express()
 app.use(bodyParser.json())
@@ -7,8 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = process.env.PORT || 3000
 
+app.use('/products', productRoutes);
+
+app.use('/orders', orderRoutes);
+
 app.get('*', (req, res) => res.status(200).send({
-   message: 'Hello World!'
+   message: 'Hello World!!'
 }))
 
 app.listen(port, () => {
